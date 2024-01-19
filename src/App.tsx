094d1efb321b639192e88c1e64586158f908583f
NicolasBronzini <nicolasbronzini7@gmail.com>
2024-01-19 12:51:47 -0300

    links

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,17 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound/NotFound"
 import Peticiones from './pages/Peticiones/peticiones';
-// import Newdevs from './pages/NewDevs/newdevs';
+ import Newdevs from './pages/NewDevs/newdevs';
 function App() {
   return (
-
     <Router>
       <Routes>
         <Route index path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/presenta-tu-proyecto" element={<Peticiones />} />
-        <Route path="/trabaja-con-nosotros" element={<Peticiones />} />
+        <Route path="/trabaja-con-nosotros" element={<Newdevs />} />
       </Routes>
     </Router>
-
   );
 }
 
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,26 +1,26 @@
-import './navbar.css'
-
+import { Link } from 'react-router-dom';
+import './navbar.css';
 
 function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar__logo">
+        <div className='ImgLogo'></div>
+      </div>
 
-    return (
-        <nav className="navbar">
-            <div className="navbar__logo">
-                <div className='ImgLogo'></div>
-            </div>
-
-            <ul className="navbar__links">
-                <li>
-                    <a href="/">Inicio</a>
-                </li>
-                <li>
-                    <a href="/presenta-tu-proyecto">Presenta tu proyecto <span>*</span></a>
-                </li>
-                <li>
-                    <a href="/trabaja-con-nosotros">Trabaja con nosotros <span>*</span></a>
-                </li>
-            </ul>
-        </nav>
-    );
+      <ul className="navbar__links">
+        <li>
+          <Link to="/">Inicio</Link>
+        </li>
+        <li>
+          <Link to="/presenta-tu-proyecto">Presenta tu proyecto <span>*</span></Link>
+        </li>
+        <li>
+          <Link to="/trabaja-con-nosotros">Trabaja con nosotros <span>*</span></Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
+
 export default Navbar;
