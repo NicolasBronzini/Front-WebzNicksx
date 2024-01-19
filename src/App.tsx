66a66d621bc3bd8b6427a462637b0caeeac088d8
NicolasBronzini <nicolasbronzini7@gmail.com>
2024-01-19 12:48:42 -0300

    debug

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound/NotFound"
 import Peticiones from './pages/Peticiones/peticiones';
 // import Newdevs from './pages/NewDevs/newdevs';
 function App() {
   return (
+
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route index path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/presenta-tu-proyecto" element={<Peticiones />} />
         <Route path="/trabaja-con-nosotros" element={<Peticiones />} />
       </Routes>
     </Router>
+
   );
 }
 
